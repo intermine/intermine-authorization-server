07552bd7bf6f0528847265d09c0ca44e133a32fd
Rahul <ry9910725@gmail.com>
2019-05-25 00:56:38 +0530

    data resource updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity, refresh_token_validity, resource_ids, authorized_grant_types, additional_information) VALUES ('flymine', '{bcrypt}$2y$10$QMLfvMcGdyEyCiIyy0.HJuenJ67NopKP6CxbPJpxOdvG.a2WU1Wqi', 'http://localhost:8080/code', 'READ,WRITE', '3600', '10000', 'inventory,payment', 'authorization_code,password,refresh_token,implicit', '{}');
+INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity, refresh_token_validity, resource_ids, authorized_grant_types, additional_information) VALUES ('flymine', '{bcrypt}$2a$10$YSsPnQqOMvgQh5zVZ4kYXexzWOUEY3GFB/4IwBWySZ.8H6Ih3YoF6', 'http://localhost:8080/code', 'READ,WRITE', '3600', '10000', 'inventory,payment', 'authorization_code,password,refresh_token,implicit', '{}');
 
 INSERT INTO PERMISSION (NAME) VALUES
   ('create_profile'),
@@ -16,8 +16,8 @@ INSERT INTO PERMISSION_ROLE (PERMISSION_ID, ROLE_ID) VALUES
   (4,1), /* delete admin */
   (2,2),  /* read operator */
   (3,2);  /* update operator */
-insert into users (id, username,password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('1', 'admin','{bcrypt}$2y$10$GUz670ztcDJvUKCIL7nvD.nRkObwFnFXnywTs8pdLtdTaGSH47NCq', 'admin@intermine.com', '1', '1', '1', '1');
-insert into  users (id, username,password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('2', 'testUser', '{bcrypt}$2y$10$gtb89utyMaaBdZxEXXxhZ.hSg0Z/CfiI3ck4SCmt/S.vHZBeBvCSq','tuser@intermine.com', '1', '1', '1', '1');
+insert into users (id, username,password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('1', 'admin','{bcrypt}$2a$10$AI4.j4/4gxmJRXjWAfyksujEAVH9COfYo272xA4bJJF9MR7C0M03u', 'admin@intermine.com', '1', '1', '1', '1');
+insert into  users (id, username,password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('2', 'testUser', '{bcrypt}$2a$10$6hmXeEQyG9vaOk.FM5XZWeoW90bKiSxlItXrwaZPoicEMAfa6bkoe','tuser@intermine.com', '1', '1', '1', '1');
 
 INSERT INTO ROLE_USER (ROLE_ID, USER_ID)
 VALUES
