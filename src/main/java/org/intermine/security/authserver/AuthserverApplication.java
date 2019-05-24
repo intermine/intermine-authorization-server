e2a518cae52d363b6f61e47422af27772762e1df
Rahul <ry9910725@gmail.com>
2019-05-24 23:05:46 +0530

    first commit

---
+++ src/main/java/org/intermine/security/authserver/AuthserverApplication.java
@@ -0,0 +1,13 @@
+package org.intermine.security.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+
+}
