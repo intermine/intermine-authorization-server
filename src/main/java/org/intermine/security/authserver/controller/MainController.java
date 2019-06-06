64449def163597621e72fc4e648ae72532be5061
Rahul <ry9910725@gmail.com>
2019-06-06 15:20:20 +0530

    User info endpoint added
    With the help of this endpoint client can fetch current logged in user information to show on the dashboard or in an application.

--- src/main/java/org/intermine/security/authserver/controller/MainController.java
+++ src/main/java/org/intermine/security/authserver/controller/MainController.java
@@ -1,14 +1,14 @@
 package org.intermine.security.authserver.controller;
 
 import org.intermine.security.authserver.model.OauthClientDetails;
+import org.intermine.security.authserver.model.Users;
 import org.intermine.security.authserver.service.CustomClientDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
@@ -21,6 +21,16 @@ public class MainController {
     @RequestMapping(value = "/client-registration", method = RequestMethod.POST)
     public Map<String, String> save(@RequestBody OauthClientDetails oauthClientDetails)throws NoSuchAlgorithmException {
         return customClientDetailsService.addCustomClientDetails(oauthClientDetails);
+    }
+    
+    @RequestMapping(value = "/user-info", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, String> currentUserName(Authentication authentication) {
+        Users user= (Users) authentication.getPrincipal();
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("username",user.getUsername());
+        map.put("email",user.getEmail());
+        return map;
     }
 
 }
