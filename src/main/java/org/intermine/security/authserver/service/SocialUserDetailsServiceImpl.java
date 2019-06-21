d88484074ff5de9cf41cafbb8e2aed883f132ed9
ry007 <ry9910725@gmail.com>
2019-06-21 14:29:10 +0530

    SocialUserDetailsServiceImpl class added
    This class implements socialuserdetailsservice and contain a single function which returns userDetails with the help of username. SocialUserDetailsService is used in our web security configuration file for creating a bean of springSecurityFilterChain.

---
+++ src/main/java/org/intermine/security/authserver/service/SocialUserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package org.intermine.security.authserver.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.social.security.SocialUserDetails;
+import org.springframework.social.security.SocialUserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SocialUserDetailsServiceImpl implements SocialUserDetailsService {
+
+    @Autowired
+    private UserDetailsService userDetailService;
+
+    @Override
+    public SocialUserDetails loadUserByUserId(String userName) throws UsernameNotFoundException, DataAccessException {
+        System.out.println("SocialUserDetailsServiceImpl.loadUserByUserId=" + userName);
+        UserDetails userDetails = ((UserDetailServiceImpl) userDetailService).loadUserByUsername(userName);
+        return (SocialUserDetailsImpl) userDetails;
+    }
+
+}
