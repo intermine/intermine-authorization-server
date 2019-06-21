8ace98bb44c42d01a292b14cb4d5deba0a57aa0c
ry007 <ry9910725@gmail.com>
2019-06-21 10:51:08 +0530

    Web util class added
    This util class will be use to user details into string with the help of toString method.

---
+++ src/main/java/org/intermine/security/authserver/utils/WebUtils.java
@@ -0,0 +1,34 @@
+package org.intermine.security.authserver.utils;
+
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class WebUtils {
+
+    public static String toString(UserDetails user) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("UserName:").append(user.getUsername());
+
+        Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+        if (authorities != null && !authorities.isEmpty()) {
+            sb.append(" (");
+            boolean first = true;
+            for (GrantedAuthority a : authorities) {
+                if (first) {
+                    sb.append(a.getAuthority());
+                    first = false;
+                } else {
+                    sb.append(", ").append(a.getAuthority());
+                }
+            }
+            sb.append(")");
+        }
+        return sb.toString();
+    }
+}
+
