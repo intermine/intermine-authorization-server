e3eecce4c1405e5fa67d80988c5a3cad27f71f6f
ry007 <ry9910725@gmail.com>
2019-07-15 14:24:40 +0530

    Methods added in client detail repository
    Find method added to fetch clients with the help of username & Save method added to update client details.

--- src/main/java/org/intermine/security/authserver/repository/ClientDetailRepository.java
+++ src/main/java/org/intermine/security/authserver/repository/ClientDetailRepository.java
@@ -3,10 +3,14 @@ package org.intermine.security.authserver.repository;
 import org.intermine.security.authserver.model.OauthClientDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClientDetailRepository extends JpaRepository<OauthClientDetails, Integer> {
     Optional<OauthClientDetails> findByClientId(String clientId);
     OauthClientDetails findByClientName(String clientName);
     OauthClientDetails findByWebsiteUrl(String websiteUrl);
+    List<OauthClientDetails> findAllByRegisteredBy(String registeredBy);
+    @Override
+    <S extends OauthClientDetails> S save(S s);
 }
