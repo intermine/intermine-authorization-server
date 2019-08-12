2fdf3e68e96aa255a5ffa88a255e1ae1a8fd3430
rahul-y <ry9910725@gmail.com>
2019-08-13 02:30:46 +0530

    schema.sql updated

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 create table if not exists  oauth_client_details (
   id SERIAL PRIMARY KEY,
-  client_id varchar(255) not null UNIQUE,
-  client_secret varchar(255) not null UNIQUE ,
+  client_id varchar(255) UNIQUE,
+  client_secret varchar(255) UNIQUE ,
   client_name VARCHAR(255) not null UNIQUE ,
   client_website_url VARCHAR(255) not null UNIQUE ,
   web_server_redirect_uri varchar(2048) default null,
